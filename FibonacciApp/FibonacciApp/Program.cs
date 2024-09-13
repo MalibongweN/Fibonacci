2aad7efc6f5f636737f47b4a77bedf0b1516e24c
Malibongwe Nzuzo Ndlovu <ST10083947@VCCONNECT.EDU.ZA>
2024-09-13 18:48:21 +0200

    Add files via upload

---
+++ FibonacciApp/FibonacciApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class FibonacciApp
+{
+    //store previously computed Fibonacci values
+    private static Dictionary<int, long> memo = new Dictionary<int, long>();
+
+   
+    public static long FibonacciMemoized(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+
+        
+        if (memo.ContainsKey(n))
+        {
+            return memo[n];
+        }
+
+       
+        long result = FibonacciMemoized(n - 1) + FibonacciMemoized(n - 2);
+        memo[n] = result;
+
+        return result;
+    }
+
+    public static void Main()
+    {
+        Console.Write("Enter the value of n to compute Fibonacci(n): ");
+        int n = int.Parse(Console.ReadLine());
+
+      
+        memo.Clear();
+
+       
+        var startTime = DateTime.Now;
+        long result = FibonacciMemoized(n);
+        var endTime = DateTime.Now;
+
+        Console.WriteLine($"Fibonacci({n}) = {result}");
+        Console.WriteLine($"Time taken with memoization: {(endTime - startTime).TotalMilliseconds} ms");
+    }
+}
